a16e94ba86f33f8360b1a7d68ea81141fa32c5ec
HTPHU0711 <109224278+HTPHU0711@users.noreply.github.com>
2022-07-13 20:37:43 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$conn = pg_connect("postgres://eqsswfxaphixgx:79c5717c086ecfdb796105255f2a0f48e0ec8b5c2278c2e680b24bb8689abc65@ec2-54-145-139-208.compute-1.amazonaws.com:5432/d200jeu77acrei");
+$conn = pg_connect("postgres://mrpplugnyshzrt:c72d992374ae2477ade571e801de311cfc6fba28683b3a9923738756a8886eff@ec2-3-223-169-166.compute-1.amazonaws.com:5432/d9n333qhs229n7");
 	echo 'Connected Successfully!!!';
 	if(!$conn)
 	{
